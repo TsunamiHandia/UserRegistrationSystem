6d3e21c288fe9c3524ed6067b0062fb9c2d6766b
TsunamiHandia <jairomtzfdz@gmail.com>
2020-11-03 13:28:34 +0100

    Initial commit

---
+++ src/test/java/com/tsunamihandia/UserRegistrationSystem/UserRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsunamihandia.UserRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
